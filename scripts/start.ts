96a131d3c39b351c8f49b43f1b70bb481da09244
lianglei-git <2838370086@qq.com>
2021-09-17 11:07:37 +0800

    add proxy

--- scripts/start.ts
+++ scripts/start.ts
@@ -2,20 +2,29 @@
 const DevServer = require('webpack-dev-server')
 const {merge} = require('webpack-merge')
 const baseConfig = require('../config/webpack.config.js')
-
+const {resolve} = require('path')
 
 module.exports = merge(baseConfig('development'), {
     devServer: {
         static: {
-            directory: '../site/index.html',
+            directory: resolve( __dirname, '../site/theme/static'),
+            watch: true,
           },
         compress: true,
-        // clientLogLevel: 'none',
-        // contentBase: '../site',
-        // watchContentBase: true,
         hot: true,
+        open:true,
         // transportMode: 'ws',
-        port: 9000,
+        port: 8080,
+        proxy: {
+            '/': { target: 'http://localhost:8080/',secure: false,
+            bypass: function (req:any) {//, res:any, proxyOptions:any
+                if (req.headers.accept.indexOf('html') !== -1) {
+                  console.log('Skipping proxy for browser request.');
+                  return '/index.html';
+                }
+              },
+        }
+        }
     }
 })
 
