3f2cec514c3e7a0a5011beb79e8b5713882a3845
lianglei-git <2838370086@qq.com>
2021-09-17 10:36:32 +0800

    使用 react-router-config 插件

--- site/theme/template/Home/App.tsx
+++ site/theme/template/Home/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
-function App() {
+import {renderRoutes} from 'react-router-config'
+import {NavLink as Link} from 'react-router-dom'
+function App({route} :any) {
   return (
     <div className="App">
       提交MAC连通性
+      <Link to="/a">跳转？？？：：A</Link>
+      {renderRoutes(route.routes)}
     </div>
   );
 }
