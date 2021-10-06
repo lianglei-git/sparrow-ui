97ffd9257d0d23194627f02b0989fa91c6bdb43d
lianglei-git <2838370086@qq.com>
2021-10-06 18:23:36 +0800

    添加动画、是否居中、嵌套modal、是否展示遮罩

--- site/theme/template/Home/index.tsx
+++ site/theme/template/Home/index.tsx
@@ -1,18 +1,40 @@
 // @ts-nocheck
-import React, { useState , useEffect} from 'react'
-
+import React, { useState, useEffect, useRef } from 'react'
+import './index.less'
 const Home = (props: any) => {
     let [shape, setshape] = useState('default')
     let [isloading, setloading] = useState(false)
     let [isloading1, setloading1] = useState(false)
     let [visible, setVisible] = useState(false)
+    let [visible2, setVisible2] = useState(false)
+    let [visible3, setVisible3] = useState(false)
+    let [visible4, setVisible4] = useState(false)
+    let modalone = useRef()
+    let modaltwo = useRef()
+    let modalthree = useRef()
+    let modalfour = useRef()
     const click = (e: any) => {
         console.log(e, 'sparrow')
     }
+    useEffect(() => {
+        console.log(modalthree)
+        modalone.current.onClose = () => {
+            setVisible(false)
+        }
+        modaltwo.current.onClose = () => {
+            setVisible2(false)
+        }
+        modalthree.current.onClose = () => {
+            setVisible3(false)
+        }
+        modalfour.current.onClose = () => {
+            setVisible4(false)
+        }
+    }, [])
     return <div>
         {/* <button onClick={e => click(e)}>99</button> */}
         主页
-        <sp-button  loading={JSON.stringify({m:123,b:123})} class="sp-button-123" onClick={e => click(e)} disabled="true"> <span>布局</span> </sp-button>
+        <sp-button loading={JSON.stringify({ m: 123, b: 123 })} class="sp-button-123" onClick={e => click(e)} disabled="true"> <span>布局</span> </sp-button>
         <button onClick={() => setshape(['circle', 'round', 'default'][Math.floor(Math.random() * 3)])}>修改类型</button>
         <hr />
         <sp-button onClick={() => setloading(!isloading)}>修改loading</sp-button>
@@ -34,15 +56,15 @@ const Home = (props: any) => {
             <sp-button size={'middle'} class="sp-button-123" >Loading</sp-button>
             <sp-button size={'small'} class="sp-button-123" >Loading</sp-button>
             <sp-button size={'mini'} class="sp-button-123" >Loading</sp-button>
-            <sp-button  size={'mini'} loading={true} class="sp-button-123" > </sp-button>
-            <sp-button shape={shape}  size={'mini'} loading={isloading1} class="sp-button-123" onClick={e => {
+            <sp-button size={'mini'} loading={true} class="sp-button-123" > </sp-button>
+            <sp-button shape={shape} size={'mini'} loading={isloading1} class="sp-button-123" onClick={e => {
                 setloading1(true)
                 setTimeout(() => {
                     setloading1(false)
                 }, 3000);
             }}> <span>click me</span></sp-button>
         </div>
-{/* 
+        {/* 
         <sp-modal title="111" appendbody>
 
             <div>说到健身房看见还是恐惧的护肤科技</div>
@@ -56,14 +78,29 @@ const Home = (props: any) => {
 
         <div className="group-modal">
             模块展示
-            <sp-button class="sp-button-modal" onClick={e => setVisible(true)} > 打开弹窗 </sp-button>
+            <sp-button onClick={e => setVisible(true)} > 打开基础弹窗 </sp-button>
+            <sp-modal ref={modalone} title="第一个modal" visible={visible}>
+                内部外部好像没办法联通
+            </sp-modal>
+
+            <sp-button onClick={e => setVisible2(true)} > 打开嵌套modal </sp-button>
+            <sp-modal ref={modaltwo} title="嵌套modal" visible={visible2} center={false}>
+                <sp-button onClick={e => setVisible3(true)} > 打开子模块 </sp-button>
+                <sp-modal ref={modalthree} title="子modal" appendbody visible={visible3} class="children-modal">
+                    嵌套子modal
+                </sp-modal>
+            </sp-modal>
+            <sp-button onClick={e => setVisible4(true)} > 打开无遮罩 </sp-button>
+            <sp-modal ref={modalfour} title="第一个modal" modal='false' center='false'  visible={visible4}>
+                没有遮罩 modal = 'false'
+            </sp-modal>
+
         </div>
-{String(visible)}
-        <sp-modal title="第一个modal" visible={visible} close={() => {console.log(123)}} on-mousemove={e => {
-            console.log(e)
-        }}>
-            内部外部好像没办法联通
-        </sp-modal>
+
+        {/* <sp-modal title="333" appendbody visible></sp-modal> 
+        <sp-modal title="444" appendbody visible></sp-modal> 
+        <sp-modal title="555" appendbody visible></sp-modal> 
+        <sp-modal title="666" appendbody visible></sp-modal>  */}
     </div>
 }
 export default Home
