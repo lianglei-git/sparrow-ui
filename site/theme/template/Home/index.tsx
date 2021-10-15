fba2e8c865e3ab3ce2dd84e66831a025d6d49823
imed <569956838@qq.com>
2021-10-15 15:00:10 +0800

    sp-modal

--- site/theme/template/Home/index.tsx
+++ site/theme/template/Home/index.tsx
@@ -42,19 +42,22 @@ const Home = (props: any) => {
                 }, 3000);
             }}> <span>click me</span></sp-button>
         </div>
-
         <sp-modal title="111" appendbody>
-
             <div>说到健身房看见还是恐惧的护肤科技</div>
         </sp-modal>
+        
 
-        <sp-modal title="222"></sp-modal>
-        <sp-modal title="333" appendbody></sp-modal>
+        {/* <sp-modal title="222"></sp-modal>
+        <sp-modal title="333" appendbody></sp-modal> */}
+
         <div>
             <span>卡点测试</span>
         </div>
 
-        <sp-modal title="第一个modal"></sp-modal>
+        {/* <sp-modal title="第一个modal" ></sp-modal>
+
+        <sp-modal title="第二个modal" title></sp-modal> */}
+
     </div>
 }
 export default Home
