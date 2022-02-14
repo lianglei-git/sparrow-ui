3e831cfb9f1d9ba10ea45d8e3db8170823d1939b
lianglei-git <2838370086@qq.com>
2022-02-14 09:00:32 +0800

    change href

--- site/theme/template/Home/index.jsx
+++ site/theme/template/Home/index.jsx
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import React, { useState, useEffect, useRef } from 'react'
+import { Link } from 'bisheng/router';
 // import ReactDOM  from 'react-dom'
 import './index.less';
 if (typeof window !== 'undefined') {
@@ -13,7 +14,7 @@ const Home = (props) => {
         <h1>Sparrow UI</h1>
         <h2>一个基于JavaScript的响应式的简洁UI组件库</h2>
         <div className='ot'>
-            <sp-button type='primary' shape="round"><a href="/components/modal/">开始使用</a></sp-button>
+            <sp-button type='primary' shape="round"><Link to="/components/modal/">开始使用</Link></sp-button>
             <sp-button shape="round"><a href="https://github.com/lianglei-git" target="_blank">Github</a></sp-button>
         </div>
         <div className="fixed-cop">
