9102da6b2988f38561c0e152ac3881fbdb0f47fd
lianglei-git <2838370086@qq.com>
2022-10-03 15:56:24 +0800

    upgrade

--- scripts/cloneFile.js
+++ scripts/cloneFile.js
@@ -4,19 +4,40 @@ const minimist = require('minimist');
 
 const argv = minimist(process.argv.slice(2));
 
+const maps = [
+    {
+        fromFileName:  path.resolve(__dirname, '../components/package.json'),
+        toFileName: path.resolve(__dirname, '../npm/package.json')
+    },
+    {
+        fromFileName:  path.resolve(__dirname, '../components/overview/index.md'),
+        toFileName: path.resolve(__dirname, '../npm/index.md')
+    },
+    {
+        fromFileName:  path.resolve(__dirname, '../LICENSE'),
+        toFileName: path.resolve(__dirname, '../npm/LICENSE')
+    },
 
-const fromFileName = path.resolve(__dirname, '../components/package.json');
-const toFileName = path.resolve(__dirname, '../npm/package.json');
-const rs = fs.createReadStream(fromFileName, {
-    autoClose: true,
-    encoding: 'utf-8',
-    highWaterMark: 64 * 1024 * 1024,
-    flags: 'r'
+]
+
+maps.map(({fromFileName, toFileName}) => {
+    try{
+        fs.rmSync(toFileName);
+    }catch(err) {
+
+    }
+    const rs = fs.createReadStream(fromFileName, {
+        autoClose: true,
+        encoding: 'utf-8',
+        highWaterMark: 64 * 1024 * 1024,
+        flags: 'r'
+    })
+    const ws = fs.createWriteStream(toFileName, {
+        encoding: 'utf-8',
+        flags: 'a',
+        highWaterMark: 16 * 1024 * 1024,
+        autoClose: true
+    })
+    rs.pipe(ws);
+    
 })
-const ws = fs.createWriteStream(toFileName, {
-    encoding: 'utf-8',
-    flags: 'a',
-    highWaterMark: 16 * 1024 * 1024,
-    autoClose: true
-})
-rs.pipe(ws);
