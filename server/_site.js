9c96b880117119ef78e20c186830bef552391665
lianglei-git <2838370086@qq.com>
2022-10-10 12:29:11 +0800

    https site 💰

--- server/_site.js
+++ server/_site.js
@@ -1,4 +1,4 @@
-const https = require('https');
+
 
 const Koa = require("koa");
 
@@ -6,21 +6,15 @@ const serve = require("koa-static");
 
 const path = require("path");
 
-const { readFileSync } = require('node:fs');
-
 const app = new Koa();
 
-const sslOptions = {
-    key: readFileSync(path.resolve(__dirname, './www.sparrowui.cn_nginx/www.sparrowui.cn.key')),
-    cert: readFileSync(path.resolve(__dirname, './www.sparrowui.cn_nginx/www.sparrowui.cn_bundle.crt')),
-}
 
 const address = require('address')
 
 
 app.use(
     serve(
-        path.join(__dirname, "../_site"),
+        path.join(__dirname, "../_site"), 
         {
             index: 'index.html', // 默认为true  访问的文件为index.html  可以修改为别的文件名或者false
             hidden: false, // 是否同意传输隐藏文件
@@ -29,9 +23,6 @@ app.use(
     )
 );
 
-
-https.createServer(sslOptions, app.callback())
-    .listen(3031, () => {
-        console.log(address.ip() + ':' + 3031);
-    });
-
+app.listen(3031, () => {
+    console.log(address.ip() +':'+ 3031);
+});
